46474a7a1ad7bcdad744ecaaf2d244132d71fc27
xbaseleet <127048636+Skybadests@users.noreply.github.com>
2023-03-06 03:27:25 +0100

    Update payment.php

--- payment.php
+++ payment.php
@@ -2,10 +2,10 @@
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
-include "includes/config.php";
+include ('includes/config.php');
 
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: login.html");
+    header("location: login.html
     exit();
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
